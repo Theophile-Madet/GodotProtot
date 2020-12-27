dfd7d7a75c815248d4f164fdfc4c0e952ec38104
Théophile MADET <theo.madet@gmail.com>
2020-12-27 20:54:43 +0100

    Added BloodCry projectiles

--- Rageborn/Attacks/BloodCry/BloodCry.gd
+++ Rageborn/Attacks/BloodCry/BloodCry.gd
@@ -2,7 +2,7 @@ extends RigidBody2D
 
 
 const SIZE := 64
-const CHARGE_TIME := 10
+const CHARGE_TIME := 4
 
 const TWEEN_X_DURATION = 1.6
 const TWEEN_Y_DURATION = 2.1
@@ -13,43 +13,46 @@ enum BloodCryState {
 }
 var current_state = BloodCryState.CHARGING
 
-const MAX_HP:= 5
+const MAX_HP:= 4
 var hp: float = MAX_HP
 var main
 
 const ATTACK_COOLDOWN := 0.3
 var time_since_last_attack: float = ATTACK_COOLDOWN
 
+var projectile_scene: PackedScene = preload("res://Rageborn/Attacks/BloodCry/BloodCryProjectile.tscn")
+
 func init(rageborne):
 	main = rageborne.main
 	var spawn_position := Vector2()
-	spawn_position.x = 100 + (main.viewport_size.x - 200) * randf()
-	spawn_position.y = 100 + (main.viewport_size.y / 2) * randf()
+	spawn_position.x = 75 + (main.viewport_size.x - 150) * randf()
+	spawn_position.y = 175 + (main.viewport_size.y / 2.2) * randf()
 	position = spawn_position
 	main.add_child(self)
 	scale = Vector2.ZERO
+	main.add_hp_bar(self)
 
 
 func _ready():
 	var tween := Tween.new()
-	tween.interpolate_property($TextureProgress, "value", 0, $TextureProgress.max_value, CHARGE_TIME, 
+	tween.interpolate_property($TextureProgress, "value", 
+			0, $TextureProgress.max_value, CHARGE_TIME, 
 			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	add_child(tween)
 	tween.start()
 	
 	tween = Tween.new()
-	tween.interpolate_property(self, "scale", Vector2.ZERO, Vector2.ONE, CHARGE_TIME, 
+	tween.interpolate_property(self, "scale", 
+			Vector2.ZERO, Vector2.ONE, CHARGE_TIME / 2, 
 			Tween.TRANS_ELASTIC, Tween.EASE_OUT)
 	add_child(tween)
 	tween.start()
 	
-	var timer := Timer.new()
-	timer.wait_time = CHARGE_TIME + 1
-	timer.connect("timeout", self, "queue_free")
-	self.add_child(timer)
-	timer.start()
+	get_tree().create_timer(CHARGE_TIME + 1).connect("timeout", self, "queue_free")
+	for delay in [0, 0.25, 0.5, 0.75]:
+		get_tree().create_timer(CHARGE_TIME + delay).connect("timeout", self, "shoot")
 	
-	timer = Timer.new()
+	var timer := Timer.new()
 	timer.wait_time = TWEEN_X_DURATION
 	timer.connect("timeout", self, "tween_background_x")
 	timer.one_shot = false
@@ -69,8 +72,9 @@ func _ready():
 func tween_background_x():
 	var tween := Tween.new()
 	tween.remove($Background, "position:x")
-	tween.interpolate_property($Background, "position:x", $Background.position.x, rand_range(-8, 8), TWEEN_X_DURATION, 
-			Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.interpolate_property($Background, "position:x", 
+			$Background.position.x, rand_range(-8, 8), 
+			TWEEN_X_DURATION, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
 	add_child(tween)
 	tween.start()
 	
@@ -78,7 +82,27 @@ func tween_background_x():
 func tween_background_y():
 	var tween := Tween.new()
 	tween.remove($Background, "position:y")
-	tween.interpolate_property($Background, "position:y", $Background.position.y, rand_range(-8, 8), TWEEN_Y_DURATION, 
-			Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.interpolate_property($Background, "position:y", 
+			$Background.position.y, rand_range(-8, 8), 
+			TWEEN_Y_DURATION, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
 	add_child(tween)
 	tween.start()
+	
+	
+func shoot():
+	var projectile = projectile_scene.instance()
+	projectile.init(self)
+	
+	
+func get_current_hp():
+	return self.hp
+	
+
+func get_max_hp():
+	return MAX_HP
+
+
+func hit(damage: float):
+	hp -= damage
+	if hp <= 0:
+		queue_free()
---
+++ Rageborn/Attacks/BloodCry/BloodCryProjectile.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+
+const SPEED := 1000
+var target: Node2D
+var direction: Vector2
+
+
+func init(bloodcry: Node2D):
+	global_position = bloodcry.global_position
+	var targets = bloodcry.main.players.duplicate()
+	targets.append(bloodcry.main.gravehold)
+	target = targets[randi() % targets.size()]
+	look_at(target.global_position)
+	direction = (target.global_position - global_position).normalized()
+	bloodcry.main.add_child(self)
+	connect("body_entered", self, "on_body_touched")
+	$VisibilityNotifier2D.connect("screen_exited", self, "queue_free")
+
+
+func _process(delta: float):
+	direction = lerp(direction, (target.global_position - global_position).normalized(), delta * 2)
+	direction = direction.normalized()
+	rotation = direction.angle()
+	position += direction * delta * SPEED
+
+
+func on_body_touched(body: RigidBody2D):
+	body.hit(1)
+	queue_free()
